42419652fe43dcb87613dee63a411cf06854987f
Ann Maria Jose <annmariajose1412@gmail.com>
2024-08-15 12:54:25 -0500

    Add counter and update reset button

--- redux-counter/src/Counter.js
+++ redux-counter/src/Counter.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { connect } from 'react-redux';
 
 class Counter extends React.Component {
     state = { count: 0 }
 
     increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        });
+        this.props.dispatch({type: 'INCREMENT'});
     }
 
     decrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        });
+        this.props.dispatch({type: 'DECREMENT'});
+    }
+
+    reset = () => {
+        this.props.dispatch({type: 'RESET'});
     }
 
     render() {
@@ -20,13 +21,20 @@ class Counter extends React.Component {
             <div>
                 <h2>Counter</h2>
                 <div>
-                    <span>{this.state.count}</span>
+                    <span>{this.props.count}</span>
                     <button onClick={this.decrement}>-</button>
                     <button onClick={this.increment}>+</button>
+                    <button onClick={this.reset}>reset</button>
                 </div>
             </div>
         )
     }
 }
 
-export default Counter;
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    };
+}
+
+export default connect(mapStateToProps)(Counter);
